635658fe4800423f0de7dafb678725886a846fcc
gelvazio <gelvazio@tidas.com.br>
2021-05-22 16:36:02 -0300

    initial

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-
-echo 'Ola Mundo Ali Baba Cloud';
+require_once 'install/control/autoload_install.php';
+$code = '735utf8PHP7';
+$filename = 'formDin';
+if (file_exists($filename)) {
+    header('Location: form.php');
+} else {
+    header('Location: install/index.php?cod='.$code);
+}
